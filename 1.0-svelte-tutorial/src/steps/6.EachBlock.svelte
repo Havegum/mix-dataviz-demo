52e5adb752fac6f1d1709b66d18bd2d58d9a7a3b
Halvard Alvheim Vegum <halvard.alvheim.vegum@bt.no>
2021-02-23 13:52:50 +0100

    ready

--- 1.0-svelte-tutorial/src/steps/6.EachBlock.svelte
+++ 1.0-svelte-tutorial/src/steps/6.EachBlock.svelte
@@ -1,7 +1,7 @@
 <script>
-let messages = [
-  { text: 'Dette er en tekst',       color: 'teal',       points:  4 },
-  { text: 'Her kommer en tekst til', color: 'salmon',     points: -3 },
-  { text: 'Siste tekst i listen!',   color: 'lightgreen', points:  1 },
+let texts = [
+  'Dette er en tekst',
+  'Her kommer en tekst til',
+  'Siste tekst i listen!',
 ];
 </script>
--- 1.0-svelte-tutorial/src/steps/8.EachComponents.svelte
+++ 1.0-svelte-tutorial/src/steps/8.EachComponents.svelte
@@ -1,1 +1,12 @@
-<!-- Post -->
+<script>
+import Post from './components/Post.svelte';
+
+let messages = [
+  { text: 'Dette er en tekst',       color: 'teal',       points:  4 },
+  { text: 'Her kommer en tekst til', color: 'salmon',     points: -3 },
+  { text: 'Siste tekst i listen!',   color: 'lightgreen', points:  1 },
+];
+</script>
+
+
+<Post text="Dette er en tekst" color="teal" points="2"/>
